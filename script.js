4e774fbff377e1f37645c96eeaa9fcabf3c4d634
chill-phil <78041889+chill-phil@users.noreply.github.com>
2021-03-16 13:11:11 -0500

    added prompts

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 // Assignment code here
-window("Alert");
-
-
-
-
-
 
 
 // Get references to the #generate element
@@ -16,7 +10,15 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+var generatePassword = function() {
+  // prompts for criteria
+  window.alert("include lower case letters?");
+  window.alert("include upper case letters?");
+  window.alert("include numbers?");
+  window.alert("include special characters? (eg. '!', '$', '@')");
+  window.prompt("password length? (between 8-128 characters, input whole number with no commas!)")
 }
 
 // Add event listener to generate button
